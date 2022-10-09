c15d0b99703385bc3cd938212c8bed25069bd300
gopipatwa <gopise73@gmail.com>
2022-10-09 16:12:58 +0530

    css

--- backend/settings.py
+++ backend/settings.py
@@ -118,6 +118,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+DISABLE_COLLECTSTATIC=1
 STATIC_URL = '/static/'
 
 # if DEBUG:
@@ -139,5 +140,3 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ]
 }
-
-DISABLE_COLLECTSTATIC=1
